7954bc215ebe4fe29c36b75bab5e0ae41def5eb8
Colby M. White <colbywhite@users.noreply.github.com>
2022-10-09 00:00:53 -0500

    feat: add print breaks

--- toPdf.js
+++ toPdf.js
@@ -4,7 +4,12 @@ const puppeteer = require('puppeteer');
 	const browser = await puppeteer.launch()
 	const page = await browser.newPage()
 	await page.goto('http://localhost:8080/', { waitUntil: ['domcontentloaded', 'load', 'networkidle0', 'networkidle2'] })
-	await page.pdf({ path: 'dist/resume.pdf', landscape: false, displayHeaderFooter: false, format: 'a4' })
+	await page.pdf({
+		path: 'dist/resume.pdf',
+		landscape: false,
+		displayHeaderFooter: false,
+		format: 'a4'
+	})
 
 	await browser.close()
 })()
