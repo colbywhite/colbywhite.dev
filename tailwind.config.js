397253aa018c7c34fa4be24fe951dd0c861ae517
Colby M. White <colbywhite@users.noreply.github.com>
2022-10-09 19:08:52 -0500

    feat: add timeline style

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 				sans: ['Segoe UI', 'Inter', ...defaultTheme.fontFamily.sans],
 				serif: ['Butler', ...defaultTheme.fontFamily.serif]
 			},
+			backgroundImage: {
+				'sharp-fade': 'linear-gradient(to top, var(--tw-gradient-from) 0, var(--tw-gradient-to) 100px, var(--tw-gradient-to) 100%);'
+			},
 			colors: {
 				'ws-blue': {
 					100: '#E6F0FF',
