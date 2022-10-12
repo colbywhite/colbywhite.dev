5416f223355e43225e7f86f62f4597dd70692c4f
Colby M. White <colbywhite@users.noreply.github.com>
2022-10-12 15:25:53 -0500

    fix: support multiple pdfs

--- toPdf.js
+++ toPdf.js
@@ -1,8 +1,11 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
 
-async function createPDF(page, path, output_filepath) {
+async function createPDF(path, output_filepath) {
+	const browser = await puppeteer.launch()
+	const page = await browser.newPage()
 	await page.goto(`http://localhost:8080/${path}`, { waitUntil: ['domcontentloaded', 'load', 'networkidle0', 'networkidle2'] })
+		.catch(e => console.error(e))
 	await page.pdf({
 		path: output_filepath,
 		landscape: false,
@@ -10,20 +13,17 @@ async function createPDF(page, path, output_filepath) {
 		format: 'a4'
 	})
 	console.log('Saved', path, 'to', output_filepath)
+	await browser.close()
 }
 
 (async () => {
-	const browser = await puppeteer.launch()
-	const page = await browser.newPage()
-	await createPDF(page, '/', 'src/resume.pdf')
+	await createPDF('/', 'src/resume.pdf')
 	const promises = fs.readdirSync('./src/covers')
 		.filter(c => c.endsWith('.md'))
 		.map(cover => ({
 			path: `covers/${cover.replace(/\.md$/, '')}`,
 			output: `src/covers/${cover.replace(/\.md$/, '.pdf')}`
 		}))
-		.map(({path, output}) => createPDF(page, path, output))
+		.map(({path, output}) => createPDF(path, output))
 	await Promise.all(promises)
-
-	await browser.close()
 })()
